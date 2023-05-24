949720490948ca649c5c4244b17da3ecdf69d106
rjzielinski <robert_zielinski1@brown.edu>
2023-05-24 11:52:36 -0400

    add smoothing_kernel()

---
+++ R/smoothing_kernel.R
@@ -0,0 +1,15 @@
+#' Smoothing Kernel for Density Estimation
+#'
+#' Implements Gaussian kernel smoothing.
+#'
+#' @param x A vector of numeric values.
+#' @param mu The mean of the Gaussian density.
+#' @param sigma The standard deviation of the Gaussian density.
+#'
+#' @return A vector of numeric values.
+#' @export
+#'
+smoothing_kernel <- function(x, mu, sigma) {
+  yseq <- dnorm((x - mu) / sigma)
+  return((sigma^(-length(x))) * prod(yseq))
+}
