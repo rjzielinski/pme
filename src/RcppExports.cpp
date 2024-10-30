770e9aba2ac7f14c62304a5f1eee3a51940cc830
Robert Zielinski <rjz1996@outlook.com>
2024-10-29 22:10:23 -0400

    documentation for updated c++ helper functions

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -34,6 +34,54 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// logspace_sum
+double logspace_sum(double lx, double ly);
+RcppExport SEXP _pme_logspace_sum(SEXP lxSEXP, SEXP lySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type lx(lxSEXP);
+    Rcpp::traits::input_parameter< double >::type ly(lySEXP);
+    rcpp_result_gen = Rcpp::wrap(logspace_sum(lx, ly));
+    return rcpp_result_gen;
+END_RCPP
+}
+// logspace_diff
+double logspace_diff(double lx, double ly);
+RcppExport SEXP _pme_logspace_diff(SEXP lxSEXP, SEXP lySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type lx(lxSEXP);
+    Rcpp::traits::input_parameter< double >::type ly(lySEXP);
+    rcpp_result_gen = Rcpp::wrap(logspace_diff(lx, ly));
+    return rcpp_result_gen;
+END_RCPP
+}
+// logspace_sum_vec
+double logspace_sum_vec(arma::vec x);
+RcppExport SEXP _pme_logspace_sum_vec(SEXP xSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::vec >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(logspace_sum_vec(x));
+    return rcpp_result_gen;
+END_RCPP
+}
+// log_smoothing_kernel
+double log_smoothing_kernel(arma::vec x, arma::vec mu, double sigma);
+RcppExport SEXP _pme_log_smoothing_kernel(SEXP xSEXP, SEXP muSEXP, SEXP sigmaSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::vec >::type x(xSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type mu(muSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma(sigmaSEXP);
+    rcpp_result_gen = Rcpp::wrap(log_smoothing_kernel(x, mu, sigma));
+    return rcpp_result_gen;
+END_RCPP
+}
 // eta_kernel
 double eta_kernel(arma::vec t, int lambda);
 RcppExport SEXP _pme_eta_kernel(SEXP tSEXP, SEXP lambdaSEXP) {
@@ -96,15 +144,49 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// calc_A
+arma::mat calc_A(arma::mat x_obs, arma::mat mu, double sigma);
+RcppExport SEXP _pme_calc_A(SEXP x_obsSEXP, SEXP muSEXP, SEXP sigmaSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type x_obs(x_obsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type mu(muSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma(sigmaSEXP);
+    rcpp_result_gen = Rcpp::wrap(calc_A(x_obs, mu, sigma));
+    return rcpp_result_gen;
+END_RCPP
+}
+// calc_weights_cpp
+arma::vec calc_weights_cpp(arma::mat x_obs, arma::mat mu, double sigma, double epsilon, int max_iter);
+RcppExport SEXP _pme_calc_weights_cpp(SEXP x_obsSEXP, SEXP muSEXP, SEXP sigmaSEXP, SEXP epsilonSEXP, SEXP max_iterSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type x_obs(x_obsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type mu(muSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma(sigmaSEXP);
+    Rcpp::traits::input_parameter< double >::type epsilon(epsilonSEXP);
+    Rcpp::traits::input_parameter< int >::type max_iter(max_iterSEXP);
+    rcpp_result_gen = Rcpp::wrap(calc_weights_cpp(x_obs, mu, sigma, epsilon, max_iter));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_pme_norm_euclidean", (DL_FUNC) &_pme_norm_euclidean, 1},
     {"_pme_dist_euclidean", (DL_FUNC) &_pme_dist_euclidean, 2},
+    {"_pme_logspace_sum", (DL_FUNC) &_pme_logspace_sum, 2},
+    {"_pme_logspace_diff", (DL_FUNC) &_pme_logspace_diff, 2},
+    {"_pme_logspace_sum_vec", (DL_FUNC) &_pme_logspace_sum_vec, 1},
+    {"_pme_log_smoothing_kernel", (DL_FUNC) &_pme_log_smoothing_kernel, 3},
     {"_pme_eta_kernel", (DL_FUNC) &_pme_eta_kernel, 2},
     {"_pme_calcE", (DL_FUNC) &_pme_calcE, 2},
     {"_pme_etaFunc", (DL_FUNC) &_pme_etaFunc, 3},
     {"_pme_calc_nearest_x", (DL_FUNC) &_pme_calc_nearest_x, 2},
     {"_pme_calc_init_param", (DL_FUNC) &_pme_calc_init_param, 3},
+    {"_pme_calc_A", (DL_FUNC) &_pme_calc_A, 3},
+    {"_pme_calc_weights_cpp", (DL_FUNC) &_pme_calc_weights_cpp, 5},
     {NULL, NULL, 0}
 };
 
