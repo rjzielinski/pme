b915b9d1f2ba7782f93b5df8bd6c449bc5f030ea
Robert Zielinski <rjz1996@outlook.com>
2024-10-29 22:08:31 -0400

    add functions for arithmetic in log-space

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,13 +1,19 @@
 #include <RcppArmadillo.h>
+#include <boost/math/distributions/normal.hpp>
 
 // [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(BH)]]
+
+
 using namespace Rcpp;
 using namespace std;
+using boost::math::normal;
 
 #include <numeric>
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <random>
 
 //' Compute the Euclidean Norm of a Vector
 //'
@@ -43,6 +49,90 @@ double dist_euclidean(arma::vec x, arma::vec y) {
   return norm_euclidean(x - y);
 }
 
+//' Sum in Logspace
+//'
+//' Given log(x) and log(y), return log(x+y)
+//'
+//' @param x A double value
+//' @param y A double value
+//' @return A numeric value
+//' @export
+// [[Rcpp::export]]
+double logspace_sum(double lx, double ly) {
+  double log_max = max(lx, ly);
+  double log_sum = log_max + log(1 + exp(-abs(lx - ly)));
+  return(log_sum);
+}
+
+//' Difference in Logspace
+//'
+//' Given log(x) and log(y), return log(x-y)
+//'
+//' @param x A double value
+//' @param y A double value
+//' @return A numeric value
+//' @export
+// [[Rcpp::export]]
+double logspace_diff(double lx, double ly) {
+  // double log_diff = lx + log(1 - exp(ly - lx));
+  double log_max = max(lx, ly);
+  double log_min = min(lx, ly);
+  double log_diff = log_max + log(1 - exp(log_min - log_max));
+  return(log_diff);
+}
+
+//' Sum over a Vector in Logspace
+//'
+//' Recursively call logspace_sum() over n > 2 values
+//'
+//' @param x A numeric vector of values in logspace
+//' @return A numeric value
+//' @export
+//'
+// [[Rcpp::export]]
+double logspace_sum_vec(arma::vec x) {
+  int n = x.n_elem;
+  double log_sum;
+  if (n >= 2) {
+    log_sum = logspace_sum(x(0), x(1));
+    if (n > 2) {
+      for (int i = 2; i < n; i++) {
+        log_sum = logspace_sum(log_sum, x(i));
+      }
+    }
+  } else {
+    throw std::range_error("x must have length >= 2.");
+  }
+  return(log_sum);
+}
+
+//' Smoothing Kernel for Density Estimation
+//'
+//' Implements Gaussian kernel smoothing on log-scale
+//'
+//' @param x A numeric vector of values.
+//' @param mu The mean of the Gaussian density
+//' @param sigma The standard deviation of the Gaussian density
+//'
+//' @return A numeric value.
+//' @export
+// [[Rcpp::export]]
+double log_smoothing_kernel(arma::vec x, arma::vec mu, double sigma) {
+  //NumericVector x_mod = as<NumericVector>(wrap(x));
+  //NumericVector mu_mod = as<NumericVector>(wrap(mu));
+  //NumericVector z_probs = dnorm((x_mod - mu_mod) / sigma);
+  //int n = x_mod.length();
+  int n = x.n_elem;
+  NumericVector z = as<NumericVector>(wrap((x - mu) / sigma));
+  NumericVector z_probs = dnorm(z);
+  double output = (-n * log(sigma)) + sum(log(z_probs));
+  // Avoid outputting infinite values for extremely small probabilities
+  if (isinf(output)) {
+    output = -DBL_MAX;
+  }
+  return(output);
+}
+
 //' Documentation Still Needed
 //'
 //' This is a function that still needs to be documented properly.
@@ -67,6 +157,8 @@ double eta_kernel(arma::vec t, int lambda) {
   }
   return y;
 }
+
+
 
 //' Documentation Still Needed
 //'
@@ -152,3 +244,84 @@ arma::mat calc_init_param(arma::mat df, arma::mat tnew, arma::vec nearest_x) {
   }
   return params;
 }
+
+//' Documentation Still Needed
+//'
+//' This is a function that still needs to be documented properly.
+//'
+//' @param df A numeric matrix.
+//' @param tnew A numeric matrix.
+//' @param nearest_x A numeric vector.
+//'
+//' @return A numeric matrix.
+// [[Rcpp::export]]
+arma::mat calc_A(arma::mat x_obs, arma::mat mu, double sigma) {
+  int n = x_obs.n_rows;
+  int N = mu.n_rows;
+  arma::mat A(n, N);
+  x_obs = x_obs.t();
+  mu = mu.t();
+  for (int j = 0; j < N; j++) {
+    for (int i = 0; i < n; i++) {
+      A(i, j) = log_smoothing_kernel(x_obs.col(i), mu.col(j), sigma);
+    }
+  }
+  return A;
+}
+
+//' Calculate Weights of Mixture Components
+//'
+//' @param x_obs A numeric matrix containing the unreduced data.
+//' @param mu A numeric matrix of component centers.
+//' @param sigma A numeric value denoting the bandwidth of the density estimation.
+//' @param epsilon A numeric value denoting the tolerance of the Euclidean distance between weights.
+//' @param max_iter An integer denoting the maximum number of iterations.
+//'
+//' @return A numeric vector of weights.
+// [[Rcpp::export]]
+arma::vec calc_weights_cpp(arma::mat x_obs, arma::mat mu, double sigma, double epsilon, int max_iter) {
+  int n = x_obs.n_rows;
+  int D = x_obs.n_cols;
+  int N = mu.n_rows;
+  arma::mat A = calc_A(x_obs, mu, sigma);
+  return(A.row(0));
+}
+
+//calc_weights <- function(x_obs, mu, sigma, epsilon = 0.001, max_iter = 1000) {
+//  # Initialize function parameters
+//  n <- nrow(x_obs)
+//  D <- ncol(x_obs)
+//  N <- nrow(mu)
+//  A <- calc_A(x_obs, mu, sigma)
+//  W <- t(t(A) + log(theta_old))
+//  theta_old <- rep(1 / N, N)
+//  abs_diff <- 10 * epsilon
+//  count <- 0
+//  lambda_hat_old <- c(n, rep(-1, D))
+
+
+//  while ((abs_diff > epsilon) & (count <= max_iter)) {
+//    # W <- t(t(A) * theta_old)
+//    W <- t(t(A) + log(theta_old))
+//    w <- Rfast::colsums(W / Rfast::rowsums(W))
+//    lambda_hat <- stats::nlm(
+//      f = f_lambda,
+//      p = lambda_hat_old,
+//      x = x_obs,
+//      mu = mu,
+//      w = w,
+//      iterlim = 1000
+//    )$estimate
+//    theta_new <- w / Rfast::rowsums(t(t(cbind(rep(1, N), mu)) * lambda_hat))
+
+//    abs_diff <- dist_euclidean(theta_new, theta_old)
+//    if (is.na(abs_diff)) {
+//      return(bound_theta(theta_old))
+//    } else {
+//      theta_old <- bound_theta(theta_new)
+//      count <- count + 1
+//      lambda_hat_old <- lambda_hat
+//    }
+//  }
+//  bound_theta(theta_new)
+//}
